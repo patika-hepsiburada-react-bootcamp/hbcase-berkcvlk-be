5cca9a4661930d2dbb5c0189e27db0997d716547
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 19:03:42 +0300

    create: filter preparer for front util

---
+++ utils/prepareFrontFilters.js
@@ -0,0 +1,53 @@
+/**
+ * Filters
+ * Prepare all filters that will be use in front
+ * Variants desctructing from dummy product data
+ */
+const prepareFrontFilters = (products) => {
+  /**
+   *
+   * @param {string} field Which field needs to desctruct to the variants
+   * @returns Variants according to given field
+   */
+  const destructVariantsBy = (field) => {
+    return (
+      products
+        // Destruct all different by field
+        .reduce((arr, { [field]: { text, value } }) => {
+          if (arr.findIndex((filt) => filt.value === value) !== -1) {
+            return arr;
+          }
+
+          return [...arr, { text, value }];
+        }, [])
+        // Calculate product count in this filter
+        .map((filt) => ({
+          ...filt,
+          count: products.filter((prod) => filt.value === prod[field].value)
+            .length,
+        }))
+    );
+  };
+
+  return [
+    {
+      title: "Renk",
+      items: destructVariantsBy("color"),
+    },
+    {
+      title: "Sıralama",
+      items: [
+        { text: "En Düşük Fiyat", value: "asc:price" },
+        { text: "En Yüksek Fiyat", value: "desc:price" },
+        { text: "En Yeniler (AZ)", value: "asc:date" },
+        { text: "En Yeniler (ZA)", value: "desc:date" },
+      ],
+    },
+    {
+      title: "Marka",
+      items: destructVariantsBy("brand"),
+    },
+  ];
+};
+
+module.exports = prepareFrontFilters;
