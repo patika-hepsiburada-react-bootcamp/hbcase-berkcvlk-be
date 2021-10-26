95469f35052fe7009014a3c44d3e094001ac7cc7
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 15:32:13 +0300

    create: util to filter products

---
+++ utils/filterProducts.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array} products All products
+ * @param {Object} filters Object that return from parseFilters
+ */
+const filterProducts = (products, filters) => {
+  return products.filter((prod) => {
+    // Initial true to check all filters return true;
+    let checked = true;
+
+    Object.entries(filters).forEach((filter) => {
+      const [name, arr] = filter;
+
+      // If any of filters doesn't include with the product
+      // related field, make boolean false. This will provide the
+      // current product item will not be in the array
+      if (!arr.includes(prod[name].value)) {
+        checked = false;
+      }
+    });
+
+    return checked;
+  });
+};
+
+module.exports = filterProducts;
