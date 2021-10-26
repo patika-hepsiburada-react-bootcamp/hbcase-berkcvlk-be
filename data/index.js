7f59ed182ad25b1c885a9540f4de91d89d873b3f
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 19:04:18 +0300

    refactor: separate dummy product data

--- data/index.js
+++ data/index.js
@@ -1,3 +1,5 @@
+const products = require("./products");
+
 /**
  * Keys to use with redis setter, getter
  */
@@ -5,71 +7,4 @@ const redisKeys = {
   products: "products",
 };
 
-/**
- * Brands
- */
-const brands = {
-  apple: "Apple",
-  samsung: "Samsung",
-};
-
-/**
- * Colors
- */
-const colors = {
-  kirmizi: "Kırmızı",
-  gumus: "Gümüş",
-};
-
-/**
- * Dummy Products
- */
-const products = [
-  {
-    id: 1,
-    title: "Apple Iphone 11",
-    brand: {
-      text: "Apple",
-      value: "apple",
-    },
-    price: 900,
-    discountRate: 12,
-    color: {
-      text: "Gümüş",
-      value: "gumus",
-    },
-    createdAt: "2021-01-12T00:00:00+03:00",
-  },
-  {
-    id: 2,
-    title: "Samsung A7 Max Pro Maxi Samsung Samsung S5 Cep Telefonu",
-    brand: {
-      text: "Samsung",
-      value: "samsung",
-    },
-    price: 800,
-    discountRate: 8,
-    color: {
-      text: "Kırmızı",
-      value: "kirmizi",
-    },
-    createdAt: "2021-02-12T00:00:00+03:00",
-  },
-  {
-    id: 3,
-    title: "Samsung A7 Max Pro Maxi Samsung Samsung S5 Cep Telefonu",
-    brand: {
-      text: "Samsung",
-      value: "samsung",
-    },
-    price: 800,
-    discountRate: 8,
-    color: {
-      text: "Gümüş",
-      value: "gumus",
-    },
-    createdAt: "2021-02-12T00:00:00+03:00",
-  },
-];
-
-module.exports = { redisKeys, products, colors, brands };
+module.exports = { redisKeys, products };
---
+++ data/products.js
@@ -0,0 +1,67 @@
+/**
+ * Dummy Products
+ */
+const products = [
+  {
+    id: 1,
+    title: "Apple Iphone 11",
+    brand: {
+      text: "Apple",
+      value: "apple",
+    },
+    price: 900,
+    discountRate: 12,
+    color: {
+      text: "Gümüş",
+      value: "gumus",
+    },
+    createdAt: "2021-01-12T00:00:00+03:00",
+  },
+  {
+    id: 2,
+    title: "Samsung A7 Max Pro Maxi Samsung Samsung S5 Cep Telefonu",
+    brand: {
+      text: "Samsung",
+      value: "samsung",
+    },
+    price: 850,
+    discountRate: 8,
+    color: {
+      text: "Kırmızı",
+      value: "kirmizi",
+    },
+    createdAt: "2021-02-12T00:00:00+03:00",
+  },
+  {
+    id: 3,
+    title: "Samsung A7 Max Pro Maxi Samsung Samsung S5 Cep Telefonu",
+    brand: {
+      text: "Samsung",
+      value: "samsung",
+    },
+    price: 800,
+    discountRate: 8,
+    color: {
+      text: "Gümüş",
+      value: "gumus",
+    },
+    createdAt: "2021-03-12T00:00:00+03:00",
+  },
+  {
+    id: 4,
+    title: "Apple Iphone 12 Pro Max",
+    brand: {
+      text: "Apple",
+      value: "apple",
+    },
+    price: 900,
+    discountRate: 12,
+    color: {
+      text: "Mavi",
+      value: "mavi",
+    },
+    createdAt: "2021-01-12T00:00:00+03:00",
+  },
+];
+
+module.exports = products;
