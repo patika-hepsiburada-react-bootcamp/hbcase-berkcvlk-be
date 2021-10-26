d7e65f18f7cd4c2b53d67200a7667e0d346c231c
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 16:47:10 +0300

    add: order products

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const { get } = require("../redis/client");
 // Utils to provide filter object and filter data by provided object
 const parseFilters = require("../utils/parseFilters");
 const filterProducts = require("../utils/filterProducts");
+const orderProducts = require("../utils/orderProducts");
 
 /* GET
  * Query can contains "filter", "search", "order"
@@ -16,16 +17,16 @@ const filterProducts = require("../utils/filterProducts");
  */
 router.get("/", async (req, res) => {
   const filters = parseFilters(req.query.filter);
+  const order = req.query.order;
 
   try {
     const response = await get(redisKeys.products);
     const products = await JSON.parse(response);
 
-    if (!filters) {
-      res.json(products);
-    }
+    const filteredProds = filterProducts(products, filters);
+    const orderedProds = orderProducts(filteredProds, order);
 
-    res.json(filterProducts(products, filters));
+    res.json(orderedProds);
   } catch (err) {
     res.status(400).json({ message: "Something gone wrong!" });
   }
