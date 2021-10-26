9aeb02c9df706a11ddb66fbf72371680973e2b56
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 19:03:57 +0300

    add: exports new utils

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,13 @@
 const filterProducts = require("./filterProducts");
 const orderProducts = require("./orderProducts");
 const parseFilters = require("./parseFilters");
+const searchProducts = require("./searchProducts");
+const prepareFrontFilters = require("./prepareFrontFilters");
 
 module.exports = {
   filterProducts,
   orderProducts,
   parseFilters,
+  searchProducts,
+  prepareFrontFilters,
 };
