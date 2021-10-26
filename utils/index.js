c1efd55109dfdbd0729bd940de8e6f2d4a4a7025
Berk Civelek <berkcvlk@gmail.com>
2021-10-26 17:05:21 +0300

    export: util indexed

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+const filterProducts = require("./filterProducts");
+const orderProducts = require("./orderProducts");
+const parseFilters = require("./parseFilters");
+
+module.exports = {
+  filterProducts,
+  orderProducts,
+  parseFilters,
+};
