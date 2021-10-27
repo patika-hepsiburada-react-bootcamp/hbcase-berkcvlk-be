17e8caa6c38464227deef2f7f0d915e903d94f20
Berk Civelek <berkcvlk@gmail.com>
2021-10-27 18:49:11 +0300

    refactor: filter item count calculation

--- utils/prepareFrontFilters.js
+++ utils/prepareFrontFilters.js
@@ -24,15 +24,17 @@ const prepareFrontFilters = (prods) => {
             return arr;
           }
 
-          return [...arr, { text, value }];
-        }, [])
+          return [
+            ...arr,
+            {
+              text,
+              value,
 
-        // Calculate product count in this filter
-        .map((filt) => ({
-          ...filt,
-          count: prods.filter((prod) => filt.value === prod[field].value)
-            .length,
-        }))
+              // Calculate product count in this filter
+              count: prods.filter((prod) => value === prod[field].value).length,
+            },
+          ];
+        }, [])
     );
   };
 
